ea9014b642d1f073be6e402e9766ec26d7c0d0bc
Dmitry862 <rk3tt@mail.ru>
2024-10-18 22:00:59 +0300

    feat: add one new solution

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -257,8 +257,11 @@ function encodeToRot13(/* str */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  // throw new Error('Not implemented');
+  if (typeof value === 'string' || value instanceof String) {
+    return true;
+  } return false;
 }
 
 
