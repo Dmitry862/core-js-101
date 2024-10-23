350f1717196adb10375cd1b4f1d259ef093085f0
Dmitry862 <rk3tt@mail.ru>
2024-10-23 21:42:37 +0300

    feat: add getFalsyValuesCount solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -363,8 +363,10 @@ function getItemsSum(arr) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  // throw new Error('Not implemented');
+  const trueArr = arr.filter(Boolean);
+  return (arr.length + 1) - (trueArr.length + 1);
 }
 
 /**
